968266336396741b6c9a9f0efed7db720940a28c
Santiago-Montiel <santmontiel@gmail.com>
2021-09-10 21:33:05 -0500

    personajes de hunter x hunter

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = ({ anime }) => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+               <h1> <a className="navbar-brand" >{anime}</a> </h1>
+                
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/components/Personajes.jsx
@@ -0,0 +1,44 @@
+export const personajes = [
+    {
+        nombre: 'Gon Freecss',
+        descripcion: 'Es el protagonista de la serie de manga y anime Hunter × Hunter. Está en búsqueda de su padre, el famoso cazador Ging Freecss.',
+        imagen: 'https://i.pinimg.com/564x/72/c7/dc/72c7dccf0b0e8ad958ab0f20d175a639.jpg'
+    },
+    {
+        nombre: 'Killua Zoldyck',
+        descripcion: 'Es el co-protagonista y el mejor amigo de Gon Freecss. Es el tercero de los cinco hijos de la familia Zoldyck.',
+        imagen: 'http://pm1.narvii.com/6362/348a81339a6db9a3b9c538326b9583db3c33bc22_00.jpg'
+    },
+    {
+        nombre: 'Kurapika',
+        descripcion: 'Es uno de los cuatro protagonistas principales de la serie Hunter x Hunter. Busca venganza por su sangriento pasado. Sus ojos brillan en un hermoso y codiciado escarlata al experimentar emociones intensas.',
+        imagen: 'http://2.bp.blogspot.com/-gqBPbEdWd48/UKvbgELTjKI/AAAAAAAAAeg/gQthQ8UXpf8/s1600/kurapica-485x401.jpeg'
+    },
+    {
+        nombre: 'Leorio Paradinight',
+        descripcion: 'Leorio es uno de los protagonistas de la serie. Cuando era niño, Leorio se dedica a convertirse en Cazador para pagar los costos necesarios de convertirse en médico y tratar a los pobres de forma gratuita.',
+        imagen: 'https://static.wikia.nocookie.net/monster-strike-enjp/images/6/65/3007.png'
+    },
+    {
+        nombre: 'Hisoka Morow',
+        descripcion: 'Hisoka es un mago y antagonista principal de Hunter x Hunter, cuyo objetivo es luchar contra los más talentosos y fuertes. Él actúa de acuerdo a su conveniencia por lo que a veces ayudará a los protagonistas o los perjudicará.',
+        imagen: 'https://www.ecured.cu/images/thumb/9/9f/Hisoka_portada.jpg/260px-Hisoka_portada.jpg'
+    },
+    {
+        nombre: 'Machi Komachine',
+        descripcion: 'Machi es la miembro N° 5 del Genei Ryodan , es una de los integrantes que estuvo desde que se fundó en ciudad meteoro (su ciudad natal), asesina la cual sigue las órdenes de su jefe.',
+        imagen: 'https://pm1.narvii.com/6582/13c7eae3fa0715399ef285b83934cbd6218bc759_hq.jpg'
+    },
+    {
+        nombre: 'Chrollo Lucilfer',
+        descripcion: 'Es el líder de la Brigada Fantasma, una pandilla infame de ladrones con recompensas de clase ',
+        imagen: 'https://pbs.twimg.com/media/Ef87rjyWAAAn7jJ.jpg'
+    },
+    {
+        nombre: 'Illumi Zoldyck',
+        descripcion: 'Illumi es el hijo mayor de Silva y Kikyo Zoldyck, y fue entrenado desde su nacimiento en el arte del asesinato. ',
+        imagen: 'https://pbs.twimg.com/profile_images/1330211456445505543/lH2DvD15.jpg'
+    }
+    
+    
+]
